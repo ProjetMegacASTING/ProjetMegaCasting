c9f72e1dd6017d605ac1fde69d8c1b2b1dcd7d4d
Alexis Lepage <alexislepage@hotmail.fr>
2014-11-24 11:07:03 +0100

    class v2

--- ProjetMegaCasting/src/Class/Contrat.java
+++ ProjetMegaCasting/src/Class/Contrat.java
@@ -10,5 +10,28 @@ package Class;
  * Alexis
  */
 public class Contrat {
+    private int id_contrat;
+    private String lib_contrat;
+
+    public int getId_contrat() {
+        return id_contrat;
+    }
+
+    public void setId_contrat(int id_contrat) {
+        this.id_contrat = id_contrat;
+    }
+
+    public String getLib_contrat() {
+        return lib_contrat;
+    }
+
+    public void setLib_contrat(String lib_contrat) {
+        this.lib_contrat = lib_contrat;
+    }
+
+    public Contrat(String lib_contrat) {
+        this.lib_contrat = lib_contrat;
+    }
+    
     
 }
